847050fde11afbfc17d39e7c80ea286ec7f40c37
Creator-Solutions <54483520+Creator-Solutions@users.noreply.github.com>
2020-05-28 01:55:13 +0200

    finished main class and game class

--- Game/Game.py
+++ Game/Game.py
@@ -2,31 +2,18 @@ import random
 
 
 class Game(object):
-    def __init__(self, score, words, word,  round, correct_word, player_name):
-        self.score = score
-        self.words = []
-        self.word = word
+    def __init__(self, name, round, wordlist):
+        self.name = name
         self.round = round
-        self.correct_word = correct_word
-        self.playername = player_name
+        self.wordlist = wordlist
 
-    def get_name(self):
-        return self.playername
+    def set_name(self):
+        return self.name
 
-    def game_round(self):
+    def set_round(self):
         return self.round
 
-    def set_game_round(self):
-        for self.correct_Word in self.words:
-            if self.word == self.correct_Word:
-                self.round += 1
-            elif not self.word == self.correct_Word:
-                self.get_word()
+    def set_list(self):
+        return self.wordlist
 
-    def get_word(self):
-        self.correct_word = random.choice(self.words)
-        return self.correct_word
-
-
-
-
+    
--- main/main.py
+++ main/main.py
@@ -1,8 +1,20 @@
 from Game.Game import Game
 import random
+import time as t
 
 
+def start_game():
+    round = 0
+    playername = input("Enter A player Name: ")
+    wordlist = ["save", "amused"]
+
+    game = Game(playername, round, wordlist)
+
+    print(f"Current Player: {game.set_name()}")
+    t.sleep(1)
+    print(f"Current Round: {game.set_round()}")
+    t.sleep(1)
+    print(f"List: {game.set_list()}")
 
 
-
-
+start_game()
